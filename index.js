b14448496da3fd0349689bcf718406d19f6e424c
Emese Balla <emese.balla@edreamsodigeo.com>
2020-10-08 18:31:16 +0200

    read time function added

--- index.js
+++ index.js
@@ -24,3 +24,33 @@ async function getRepos() {
 getRepos()
 
 
+
+window.onload = function readTime() {
+  const fullText = document.querySelector('body').innerText
+  const wordsPerMinute = 265 // WPM - English
+  const wordCount = fullText.split(/\s/g).length
+  const minutes = wordCount / wordsPerMinute
+  const readTime = Math.ceil(minutes)
+  console.log(readTime + ' min read');
+  document.getElementById('minute').innerText = readTime + ' min read';
+  document.getElementById('minute').style.color = 'orange';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
